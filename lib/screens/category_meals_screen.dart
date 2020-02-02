1be222997c6c900c804f7e75e89130d8c35cf6c0
anandagr21 <aggarwal.anand14@gmail.com>
2020-02-02 19:32:15 +0530

    displaying recipe item and imageURL

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,3 +1,4 @@
+import '../widgets/meal_item.dart';
 import 'package:flutter/material.dart';
 import '../dummy_data.dart';
 
@@ -25,7 +26,13 @@ class CategoryMealScreen extends StatelessWidget {
         ),
         body: ListView.builder(
           itemBuilder: (ctx, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              affordability: categoryMeals[index].affordability,
+              complexity: categoryMeals[index].complexity,
+              duration: categoryMeals[index].duration,
+            );
           },
           itemCount: categoryMeals.length,
         ),
