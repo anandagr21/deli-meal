33f90bb9c8d6cc2c63d86c0a0c5ab78e207c62f6
anandagr21 <aggarwal.anand14@gmail.com>
2020-02-03 23:27:17 +0530

    bottom tabbar added

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,5 +1,4 @@
 import './favorites_screen.dart';
-
 import './categories_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,37 +8,81 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorite',
+    },
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-//          backgroundColor: Colors.red,
-          bottom: TabBar(
-//            indicatorColor: Colors.black54,
-
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.category,
-                ),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.star,
-                ),
-                text: 'Favorites',
-              ),
-            ],
+//    return DefaultTabController(
+//      length: 2,
+//
+//      child: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Meals'),
+////          backgroundColor: Colors.red,
+//          bottom: TabBar(
+////            indicatorColor: Colors.black54,
+//
+//            tabs: <Widget>[
+//              Tab(
+//                icon: Icon(
+//                  Icons.category,
+//                ),
+//                text: 'Categories',
+//              ),
+//              Tab(
+//                icon: Icon(
+//                  Icons.star,
+//                ),
+//                text: 'Favorites',
+//              ),
+//            ],
+//          ),
+//        ),
+//        body: TabBarView(children: <Widget>[
+//          CategoriesScreen(),FavoritesScreen()
+//        ],),
+//      ),
+//    );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Category'),
           ),
-        ),
-        body: TabBarView(children: <Widget>[
-          CategoriesScreen(),FavoritesScreen()
-        ],),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.favorite),
+            title: Text('Favorites'),
+          ),
+        ],
       ),
     );
   }
